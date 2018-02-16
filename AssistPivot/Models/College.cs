faef2a807e2ed123ffdb139f2c0aba62227a40be
hobblyhoy <hobblyhoy@gmail.com>
2018-02-16 10:35:00 -0800

    glimpse and a little ef init

---
+++ AssistPivot/Models/College.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AssistPivot.Models
+{
+    public class College
+    {
+        public int CollegeId { get; set; }
+        public string Name { get; set; }
+        public string Shorthand { get; set; }
+    }
+
+    //public class CollegeContext : DbContext
+    //{
+    //    public DbSet<College> Colleges { get; set; }
+    //}
+}
